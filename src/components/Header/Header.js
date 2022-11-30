9bbda014a679336fd136e0c9e8d5e54d20e31aff
thejaduijin <lokendra.06.work@gmail.com>
2022-11-30 19:06:49 +0530

    Header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,53 @@
-import React from 'react'
-import { Container, FormControl, Navbar } from 'react-bootstrap'
-import Dropdown from 'react-bootstrap/Dropdown';
+import React from "react";
+import { Badge, Container, FormControl, Navbar } from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
     return (
-        <Navbar bg="dark" variant="dark" style={{
-            height: 80
-        }}>
+        <Navbar
+            bg="dark"
+            variant="dark"
+            style={{
+                height: 80,
+            }}
+        >
             <Container>
                 <Navbar.Brand>
-                    <a href='/'>Shopping Cart</a>
+                    <a href="/">Shopping Cart</a>
                 </Navbar.Brand>
-                <Navbar.Text className='search'>
-                    <FormControl style={{
-                        width: 500
-                    }} placeholder="Search for Products" className='m-auto' />
+                <Navbar.Text className="search">
+                    <FormControl
+                        style={{
+                            width: 500,
+                        }}
+                        placeholder="Search for Products"
+                        className="m-auto"
+                    />
                 </Navbar.Text>
                 <Dropdown>
                     <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        Dropdown
+                        <FaShoppingCart color="white" fontSize="25px" />
+                        <Badge>{10}</Badge>
                     </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    <Dropdown.Menu
+                        style={{
+                            minWidth: 370,
+                        }}
+                    >
+                        <span
+                            style={{
+                                padding: 10,
+                            }}
+                        >
+                            Cart is Empty!
+                        </span>
                     </Dropdown.Menu>
                 </Dropdown>
             </Container>
         </Navbar>
-    )
-}
+    );
+};
 
-export default Header
-
+export default Header;
